aa9cb5b78aa1c89a7d75b0cc20773e9484b055a4
Ikhsan <ikhsan.ssr@gmail.com>
2020-07-03 21:22:56 +0700

    view

--- application/views/ktp/list.php
+++ application/views/ktp/list.php
@@ -1,17 +1,31 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<nav class="navbar navbar-dark bg-dark text-center"">
+  <!-- Navbar content -->
+  <h1 class="text-center" style="color: lightgrey;">KELOMPOK 6</h1>
+  <h2 style="color: lightgrey;">Ikhsan, Robin, Bayu, Ade</h2>
+</nav>
+
+
+
 <font color="orange">
 <?php echo $this->session->flashdata('hasil'); ?>
 </font>
-<table border="1">
+
+<div class="card ">
+  <div class="card-body shadow p-3 mb-5 bg-white rounded">
+
+<table class="table">
+  <thead class="thead-dark">
     <tr>
-    <th>NIK</th>
-    <th>NAMA</th>
-    <th>JK</th>
-    <th>Tempat Lahir</th>
-    <th>Tanggal Lahir</th>
-    <th>Agama</th>
-    <th>Kenegaraan</th>
-    <th>Pekerjaan</th>
-    <th>Pendidikan</th>
+    <th scope="col">NIK</th>
+    <th scope="col">NAMA</th>
+    <th scope="col">JK</th>
+    <th scope="col">Tempat Lahir</th>
+    <th scope="col">Tanggal Lahir</th>
+    <th scope="col">Agama</th>
+    <th scope="col">Kenegaraan</th>
+    <th scope="col">Pekerjaan</th>
+    <th scope="col">Pendidikan</th>
  
     </tr>
     <?php
@@ -31,3 +45,6 @@
     }
     ?>
 </table>
+
+</div>
+</div>
